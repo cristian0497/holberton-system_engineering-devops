5edce4af7f8aa9baf0990726baf4ef65c63d8962
cristian0497 <cristian04.97@hotmail.com>
2020-03-23 19:25:37 -0500

    [FIX] Style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Script to create a new file with puppet
-file { '/tmp/holberton':
-ensure => file,
-path   => '/tmp/holberton',
-owner  => 'www-data',
-group  => 'www-data',
-mode   => '0744',
-contet => 'I love Puppet',
+  file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
