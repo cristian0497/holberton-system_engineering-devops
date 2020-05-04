eb1dfdc88d58cd672bd42d1bca9cf70614b9cd8f
cristian0497 <cristian04.97@hotmail.com>
2020-05-04 23:02:47 +0000

    [ADD] Pep8 Fix, and order alphabetical

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,17 +2,19 @@
 """ JSON Plcae Holder request """
 import csv
 import json
-import urllib.request
 import sys
+import urllib.request
 
 
 def main():
-    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    """ Export request to CSV file """
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        sys.argv[1])
     with urllib.request.urlopen(users_url) as Users:
         users = Users.read().decode('utf-8')
         users_dic = json.loads(users)
-
-    users_url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(sys.argv[1])
+    users_url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(
+        sys.argv[1])
     with urllib.request.urlopen(users_url) as ToDos:
         list_task = []
         todos = ToDos.read().decode('utf-8')
@@ -21,7 +23,8 @@ def main():
     name = "{}.csv".format(sys.argv[1])
     with open(name, 'w', newline='') as file_csv:
         return_value = []
-        writer = csv.writer(file_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(file_csv, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
         for task in todos_dic:
             ret = []
             ret.append(str(users_dic.get('id')))
@@ -30,6 +33,6 @@ def main():
             ret.append(task.get('title'))
             return_value.append(ret)
             writer.writerow(ret)
-        
+
 if __name__ == "__main__":
     main()
