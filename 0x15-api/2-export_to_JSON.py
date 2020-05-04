af79647bc472b9dfe0524bea1aa0d639cdcf33d0
Cristian <cristian@Blue>
2020-05-04 15:59:45 -0500

    [ADD] pep8 pending,

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" JSON Plcae Holder request """
+import csv
+import json
+import urllib.request
+import sys
+
+
+def main():
+    """ Main method to does a request and return a JSON file """
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    with urllib.request.urlopen(users_url) as Users:
+        users = Users.read().decode('utf-8')
+        users_dic = json.loads(users)
+    users_url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(sys.argv[1])
+    with urllib.request.urlopen(users_url) as ToDos:
+        list_task = []
+        todos = ToDos.read().decode('utf-8')
+        todos_dic = json.loads(todos)
+    name = "{}.json".format(sys.argv[1])
+    with open(name, 'w') as file_json:
+        return_value = {}
+        ret = []
+        for task in todos_dic:
+            tmp = {}
+            tmp['task'] = task.get('title')
+            tmp['completed'] = task.get('completed')
+            tmp['username'] = users_dic.get('username')
+            ret.append(tmp)
+        return_value[sys.argv[1]] = ret
+        json.dump(return_value, file_json)
+
+if __name__ == "__main__":
+    main()
