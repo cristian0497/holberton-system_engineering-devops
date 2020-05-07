5c096ab82691235b94f41087fcebe291bf072206
Cristian <cristian@Blue>
2020-05-07 13:29:07 -0500

    [ADD] Try except added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ def number_of_subscribers(subreddit):
     """ A resquest to API by subreddit """
     base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     data = {'user-agent': 'Scrap/0.0.1'}
-    res = requests.get(base_url, headers=data, allow_redirects=False).json()
-    if res.get('error') == 404:
-        return 0
-    else:
+    try:
+        res = requests.get(base_url, headers=data, allow_redirects=False).json()
         return(res.get('data').get('subscribers'))
+    except:
+        return 0
