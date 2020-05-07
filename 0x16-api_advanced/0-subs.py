516fa1406f446120cbdaa1948e1262602a4c3e33
Cristian <cristian@Blue>
2020-05-07 13:38:33 -0500

    [ADD] Fix pep8 Style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     data = {'user-agent': 'Scrap/0.0.1'}
     try:
-        res = requests.get(base_url, headers=data, allow_redirects=False).json()
+        res = requests.get(base_url, headers=data,
+                           allow_redirects=False).json()
         return(res.get('data').get('subscribers'))
     except:
         return 0
