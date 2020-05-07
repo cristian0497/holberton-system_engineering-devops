4727e98f940a00b143e0236baee47aba91531e9e
Cristian <cristian@Blue>
2020-05-07 13:20:34 -0500

    [ADD] comments added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """ A resquest to API by subreddit """
     base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     data = {'user-agent': 'Scrap/0.0.1'}
     res = requests.get(base_url, headers=data, allow_redirects=False).json()
