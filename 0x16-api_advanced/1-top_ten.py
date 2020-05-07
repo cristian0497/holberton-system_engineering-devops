9cb93365486984d01bd99d6f475456decd8e2e9c
Cristian <cristian@Blue>
2020-05-07 18:15:46 -0500

    [ADD]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,20 +5,18 @@ import requests
 
 def top_ten(subreddit):
     """ The top ten of the hostpost """
-    url = 'https://www.reddit.com/subreddits/search.json'
-    par = {'q': subreddit, 'sort': 'relevance', 'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     data = {'user-agent': 'scraping-0.0.2'}
     try:
-        res = requests.get(url, headers=data, params=par,
-                           allow_redirects=False).json()
+        res = requests.get(url, headers=data, allow_redirects=False).json()
         query = res.get('data').get('children')
         result = []
         for child in query:
             data = child.get('data')
             result.append(data.get('title'))
         if len(result) != 0:
-            for val in result:
-                print(val)
+            for val in range(0, 10):
+                print(result[val])
         else:
             print(None)
     except:
