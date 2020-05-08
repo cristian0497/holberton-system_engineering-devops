10e776187b211b3e47848d997d1477a376cff98e
Cristian <cristian@Blue>
2020-05-07 23:47:05 -0500

    [ADD] new version

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ comparations with API request title """
+import operator
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], after='', values=[]):
+def count_words(subreddit, word_list, hot_list=[], after='', values={}):
     """ Request with recursion and compare the response """
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
         subreddit, after)
     data = {'user-agent': 'scraping_rec-0-0-3'}
     if len(hot_list) == 0:
         for x in range(0, len(word_list)):
-            values.append(0)
+            values[word_list[x]] = 0
     try:
         res = requests.get(url, headers=data,
                            allow_redirects=False).json()
@@ -20,12 +21,14 @@ def count_words(subreddit, word_list, hot_list=[], after='', values=[]):
             data = child.get('data').get('title')
             for x in range(0, len(word_list)):
                 if word_list[x].lower() in data.lower():
-                    values[x] += 1
+                    values[word_list[x]] += 1
             hot_list.append(data)
         if after is None:
-            for x in range(0, len(word_list)):
-                if values[x] > 0:
-                    print("{}: {}".format(word_list[x], values[x]))
+            values_sort = sorted(values.items(), key=operator.itemgetter(1))
+            for n in range(len(values_sort), 0, -1):
+                if values_sort[n - 1][1] > 0:
+                    print("{}: {}".format(values_sort[n - 1][0],
+                                          values_sort[n - 1][1]))
             return(hot_list)
         return(count_words(subreddit, word_list,  hot_list, after, values))
     except:
