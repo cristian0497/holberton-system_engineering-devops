a0e52c1f179e05158c87b54fd7e33c9eca28494c
cristian0497 <cristian04.97@hotmail.com>
2020-05-19 12:40:10 -0500

    [ADD] web stack debugging project

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+exec { '/bin/bash':
+command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+creates => '/var/www/html/wp-includes/class-wp-locale.phpp',
+path    => ['/usr/bin', '/usr/sbin',],
+}
+
+service { 'apache2':
+ensure  => running,
+restart => 'sudo service apache2 restart',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,6 @@
+# Project Web Stack debuggin 3
+Strace command, used to list all syscalls of the specific process,
+ps aux to list al process, and Puppet!, How to script a Puppet script
+to fix the apache2 server process
+Written by: Cristian Hurtado Diaz - 
+Software Engineer Holberton School
