844ba194c6ca45070de654e9f76afd42c317881b
cristian0497 <cristian04.97@hotmail.com>
2020-05-20 11:37:29 -0500

    [ADD] Add a comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Change the name of a file with puppet to fix the apache server
 exec { '/bin/bash':
 command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
 creates => '/var/www/html/wp-includes/class-wp-locale.phpp',
