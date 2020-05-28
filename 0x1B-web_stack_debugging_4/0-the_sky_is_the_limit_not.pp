fd8eeaaea4610f73b83237383446e27896b56686
cristian0497 <cristian04.97@hotmail.com>
2020-05-28 13:48:22 -0500

    [ADD] Restart command added to first command,

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # script to add more worker process to nginx .conf file and restart service
 
 exec { 'sed command':
-command => 'sudo sed -i "5 s/[0-9]\+/$(ulimit -n)/" /etc/default/nginx',
+command => 'sudo sed -i "5 s/[0-9]\+/$(ulimit -n)/" /etc/default/nginx; sudo service nginx restart',
 path    => ['/usr/bin/', '/usr/sbin'],
 }
 exec { 'nginx restart':
