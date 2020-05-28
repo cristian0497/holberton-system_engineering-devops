9b328eb17206b25a70ab64f90f85777166e81e28
cristian0497 <cristian04.97@hotmail.com>
2020-05-28 12:37:48 -0500

    [FEATURE] new structure

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,14 @@
 # script to add more worker process to nginx .conf file and restart service
 
-exec { '/bin':
-command => "sudo sed -i 's/worker_processes [0-9]/worker_processes 10/' /etc/nginx/nginx.conf",
+exec { 'nginx stop':
+command => 'service nginx stop',
+path    => '/usr/bin/',
+}
+exec { 'sed command':
+command => 'sudo sed -i 's/worker_processes [0-9]/worker_processes 10/' /etc/nginx/nginx.conf',
 path    => ['/usr/bin/', '/usr/sbin'],
 }
-service { 'nginx':
-restart => 'sudo service nginx restart',
+exec { 'nginx start':
+command => 'sudo service nginx start',
+path    => '/usr/bin/',
 }
